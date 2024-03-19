f872da292b448ef4afcede6047d8c21181a41271
Ossama Tammam <ossamatammam02@gmail.com>
2024-03-19 02:44:10 +0200

    feature

--- src/AI.py
+++ src/AI.py
@@ -1,3 +1,15 @@
 class AI:
     def __init__(self, board):
         self.board = board
+
+    def getValidMoves(self):
+        validLocations = []
+
+        for col in range(self.board.maxCols):
+            if self.board.isValidLocation(col):
+                validLocations.append(col)
+
+        return validLocations
+
+    def minMax(depth, alpha, beta, maximizingPlayer):
+        pass
